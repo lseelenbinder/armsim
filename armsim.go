2f04d3bc722ff05f5f547c705516fa1ab9e16a82
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-09-04 19:41:29 -0400

    Finish implementation of command line flags.

--- armsim.go
+++ armsim.go
@@ -1,12 +1,52 @@
 package main
 
-import "fmt"
+import (
+  "flag"
+  "fmt"
+  "log"
+)
 
 type Options struct {
-  fileName string
-  memorySize uint32
+  fileName   string
+  memorySize uint
 }
 
 func main() {
-  fmt.Println("Welcome to ARMSim by Luke Seelenbinder.")
+  // Setup Logging
+  log.SetPrefix("Main: ")
+
+  // Welcome the user
+  fmt.Println("ARMSim by Luke Seelenbinder.")
+
+  // Handle command line flags
+  options := processFlags()
+
+  print(options)
+}
+
+func processFlags() *Options {
+  // Create Options
+  options := new(Options)
+
+  // Define Options
+  flag.UintVar(&options.memorySize, "mem-size", 32768, "RAM size in bytes")
+  flag.StringVar(&options.fileName, "load", "", "ELF File Name")
+
+  // Parse Options
+  flag.Parse()
+
+  // Validate Options
+  log.Println("RAM Size:", options.memorySize)
+  if options.memorySize > 1048576 {
+    fmt.Println("RAM size is too large. Must be under 1MB (1048576).")
+    log.Fatalln("RAM size is too large. Quiting...")
+  }
+
+  log.Println("File name:", options.fileName)
+  if options.fileName == "" {
+    fmt.Println("Please specify a file name.")
+    log.Fatalln("Must specify a file name. Quiting...")
+  }
+
+  return options
 }
--- journal.markdown
+++ journal.markdown
@@ -3,3 +3,8 @@
 
 # Preparation (Total time: 2 hours)
 1. Investigate GO as a possibility for the project. (Time: 2 hours)
+
+# Bootstrapping Code (Total time: 0.75 hours)
+1. Implement main() and command line flag parsing. (Time: 0.75 hours)
+  - [http://golang.org/pkg/log/]()
+  - [http://golang.org/pkg/flag/]()
