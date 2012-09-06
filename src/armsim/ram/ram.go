d13597cccceea3334ca00ec7fc01f5ab85608969
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-09-05 22:05:14 -0400

    Add ReadByte and WriteByte to armsim/ram.

--- src/armsim/ram/ram.go
+++ src/armsim/ram/ram.go
@@ -7,8 +7,36 @@ type RAM struct {
 }
 
 func Init(nBytes uint32) RAM {
+  log.SetPrefix("RAM: ")
   log.Println("Initializing", nBytes, "bytes of RAM...")
   ram := RAM{make([]byte, nBytes)}
 
   return ram
+}
+
+func (r *RAM) WriteByte(address uint32, data byte) bool {
+  log.SetPrefix("RAM: ")
+
+  defer func() {
+    recover()
+    if address > uint32(len(r.memory)) {
+      log.Println("ERROR: Could not write byte. Address is out of range.")
+    }
+  }()
+
+  r.memory[address] = data
+  return true
+}
+
+func (r *RAM) ReadByte(address uint32) (byte, bool) {
+  log.SetPrefix("RAM: ")
+
+  defer func() {
+    recover()
+    if address > uint32(len(r.memory)) {
+      log.Println("ERROR: Could not read byte. Address is out of range.")
+    }
+  }()
+
+  return r.memory[address], true
 }
--- src/armsim/ram/ram_test.go
+++ src/armsim/ram/ram_test.go
@@ -11,6 +11,7 @@ func TestInit(t *testing.T) {
 
 // Helper method for the RAM initializer testing code
 func runInit(t *testing.T, nBytes uint32) {
+  // Setup
   ram := Init(nBytes)
 
   // Ensure RAM is the same length as nBytes
@@ -23,5 +24,42 @@ func runInit(t *testing.T, nBytes uint32) {
     if ram.memory[i] != byte(0) {
       t.Fatal("Did not initalize RAM sector", i)
     }
+  }
+}
+
+func TestWriteByte(t *testing.T) {
+  // Setup
+  ram := Init(32)
+  var byteToWrite byte = 95;
+
+  // Test under normal conditions
+  result := ram.WriteByte(0, byteToWrite)
+  if result == true && byteToWrite != ram.memory[0] {
+    t.Fatal("Did not properly write byte.")
+  }
+
+  // Test address out of range
+  result = ram.WriteByte(33, byteToWrite)
+  if result == true {
+    t.Fatal("Attempted to write out of range address.")
+  }
+}
+
+func TestReadByte(t *testing.T) {
+  // Setup
+  ram := Init(32)
+
+  // Test normal reading of bytes
+  ram.WriteByte(0, 95)
+  b, result := ram.ReadByte(0)
+  if result && b != 95 {
+    t.Fatal("Byte not read properly.")
+  }
+
+  // Test address out of range
+  ram.WriteByte(1, 95)
+  b, result = ram.ReadByte(80)
+  if result {
+    t.Fatal("Attempted to read out of range address.")
   }
 }
