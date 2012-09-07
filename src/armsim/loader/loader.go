86e0f2e6d09faf20583610683445d991208aba04
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-09-07 00:46:56 -0400

    Add LoadELF().

--- src/armsim/loader/loader.go
+++ src/armsim/loader/loader.go
@@ -1,1 +1,79 @@
 package loader
+
+import (
+  "armsim/ram"
+  "os"
+  "debug/elf"
+  "encoding/binary"
+  "log"
+)
+
+func LoadELF(filePath string, memory *ram.RAM) bool {
+  log.SetPrefix("Loader: ")
+  // Attempt to open file
+  log.Printf("Opening file %s", filePath)
+  file, err := os.Open(filePath)
+  if err != nil {
+    log.Printf("Error reading file (perhaps it doesn't exist)...")
+    return false
+  }
+
+  // Test magic bytes
+  log.Println("Testing magic bytes...")
+  magic := [4]byte{}
+  err = binary.Read(file, binary.LittleEndian, &magic)
+  log.Printf("Magic bytes: %s", magic)
+
+  if err != nil || magic[0] != 0x7f || magic[1] != 'E' || magic[2] != 'L' || magic[3] != 'F' {
+    log.Println("ELF magic bytes were incorrect.")
+    return false
+  }
+
+  // Read ELF Header
+  log.Println("Reading ELF header...")
+  file.Seek(0,0)
+  elfHeader := new(elf.Header32)
+  err = binary.Read(file, binary.LittleEndian, elfHeader)
+  if err != nil {
+    log.Println("Error reading ELF header...")
+    return false
+  }
+
+  log.Printf("Program header offset: %d", elfHeader.Phoff)
+  log.Printf("# of program header entires: %d", elfHeader.Phnum)
+
+
+  // Seek to Program Header start
+  file.Seek(int64(elfHeader.Phoff), 0)
+
+  // Read Program Headers
+  log.Println("Reading program headers...")
+  pHeader := new(elf.Prog32)
+  for i := 0; uint16(i) < elfHeader.Phnum; i++ {
+    // Seek to program header
+    offset := int64(elfHeader.Phoff) + int64(i) * int64(elfHeader.Phentsize)
+    file.Seek(offset, 0)
+
+    // Read program header
+    err = binary.Read(file, binary.LittleEndian, pHeader)
+    if err != nil {
+      log.Println("Error reading program header %d...", i)
+      return false
+    }
+
+    log.Printf("Reading program header %d of %d - Offset: %d, Size: %d, Address: %d", i + 1, elfHeader.Phnum, pHeader.Off, pHeader.Filesz, pHeader.Vaddr)
+    // Seek to offset
+    file.Seek(int64(pHeader.Off), 0)
+
+    // Read to RAM
+    b := make([]byte, 1)
+    var i uint32 = 0
+    for ; i < pHeader.Filesz; i++ {
+      file.Read(b)
+      memory.WriteByte(pHeader.Vaddr + i, b[0])
+    }
+  }
+
+  file.Close()
+  return true
+}
---
+++ src/armsim/loader/loader_test.go
@@ -0,0 +1,53 @@
+package loader
+
+import (
+  "testing"
+  "armsim/ram"
+)
+
+func TestLoadELF(t *testing.T) {
+  // Setup RAM
+  memory := ram.Init(32 * 1024)
+
+  // Load Non-existent Test File
+  success := LoadELF("asdfasdfaitheirhasdifhadf", &memory)
+  checksum := memory.Checksum()
+  if success {
+    t.Fatalf("should have failed without success")
+  }
+
+  // Load Non-ELF Test File
+  success = LoadELF("loader.go", &memory)
+  checksum = memory.Checksum()
+  if success {
+    t.Fatalf("should have failed without magic number success")
+  }
+
+  // Load Test File 1
+  memory = ram.Init(32 * 1024)
+  success = LoadELF("../../../test/test1.exe", &memory)
+  checksum = memory.Checksum()
+  if !success || checksum != 536861081 {
+    t.Fatalf("Checksum did not match for test1.exe. Expected 536861081. Got %d", checksum)
+  }
+
+  // Clear RAM
+  memory = ram.Init(32 * 1024)
+
+  // Load Test File 2
+  LoadELF("../../../test/test2.exe", &memory)
+  checksum = memory.Checksum()
+  if !success || checksum != 536864433 {
+    t.Fatalf("Checksum did not match for test2.exe. Expected 536864433. Got %d", checksum)
+  }
+
+  // Clear RAM
+  memory = ram.Init(32 * 1024)
+
+  // Load Test File 3
+  LoadELF("../../../test/test3.exe", &memory)
+  checksum = memory.Checksum()
+  if !success || checksum != 536861199 {
+    t.Fatalf("Checksum did not match for test3.exe. Expected 536861199. Got %d", checksum)
+  }
+}
