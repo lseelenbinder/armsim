4014e49bbc568b2ee04751ac3f22b426f4d8dbd8
Anas Khan <anas.khan96@outlook.com>
2017-01-28 19:26:19 +0530

    changing name to check go get

--- armsim.go
+++ armsim.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/lseelenbinder/armsim/armsim"
-	"github.com/lseelenbinder/armsim/web"
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/anaskhan96/armsim/armsim"
+	"github.com/anaskhan96/armsim/web"
 	"log"
 	"os"
 	"os/exec"
